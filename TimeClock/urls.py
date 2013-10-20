96355fb5158b4571eec0d84edaeb7bd7c6da2226
Travis Hunt <tdhunt631@gmail.com>
2013-10-20 13:54:26 -0600

    initial commit

---
+++ TimeClock/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'TimeClock.views.home', name='home'),
+    # url(r'^TimeClock/', include('TimeClock.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
